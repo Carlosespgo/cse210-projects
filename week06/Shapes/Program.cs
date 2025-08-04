1392c0c04122f2a416b8ccd9e9c01fcd7bbb0259
Carlosespgo <cebc2224@gmail.com>
2025-08-04 16:17:05 -0500

    finish learning activity week 6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> _shapes= new List<Shape>();
+        _shapes.Add(new Square("Red", 2));
+        _shapes.Add(new Rectangle(5, 6, "green"));
+        _shapes.Add(new Circle(5, "Yellow"));
+
+        foreach (Shape shape in _shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");  
+        }
     }
 }
